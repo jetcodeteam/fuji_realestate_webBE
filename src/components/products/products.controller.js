63302a7e9fb3424b3980120fc234190f86fb3bc1
Shekcon <quangsang9773@gmail.com>
2019-08-27 15:18:57 +0700

    + Refactor code + Update document swagger + Bug of swagger cann't upload multiple files

--- src/components/products/products.controller.js
+++ src/components/products/products.controller.js
@@ -1,18 +1,22 @@
+/* eslint-disable no-console */
 const router = require('express').Router();
+const uploadFile = require('../../middlewares/multer');
 
 router.get('', (req, res) => {
   return res.send('Get all products');
 });
 
-router.post('', (req, res) => {
-  return res.send('Create a new product');
+router.post('', uploadFile.array('images', 10), (req, res) => {
+  console.log(req.files);
+  console.dir(req.body.images);
+  return res.status(201).send('Create a new product');
 });
 
 router.get('/:id', (req, res) => {
   return res.send('Get one products');
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', uploadFile.array('images', 10), (req, res) => {
   return res.send('Update a product');
 });
 
