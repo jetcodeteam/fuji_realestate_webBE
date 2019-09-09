158a390c82c76f229801eac367bfea847f137bec
Shekcon <quangsang9773@gmail.com>
2019-09-09 14:14:47 +0700

    + Fix ckdeditor 5

--- src/apis/uploads/uploads.controller.js
+++ src/apis/uploads/uploads.controller.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const uploadFile = require('../../middlewares/multer');
 
-router.post('', uploadFile.single('image'), (req, res) => {
+router.post('', uploadFile.single('upload'), (req, res) => {
   res.send({
     filename: `${req.file.filename}`,
     url: `${req.protocol}://${req.headers.host}/static/${req.file.filename}`,
