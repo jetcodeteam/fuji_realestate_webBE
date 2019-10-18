3bebe2d76b92409c73e6aa1db4ce275c0b79d05a
Shekcon <quangsang9773@gmail.com>
2019-10-18 15:20:36 +0700

    + Update swagger with new API districts, wards

--- src/apis/locations/wards.controller.js
+++ src/apis/locations/wards.controller.js
@@ -1,17 +1,9 @@
-const passport = require('passport');
 const router = require('express').Router();
 const crud = require('../../utils/crud');
 const Wards = require('./ward');
-const authorizeAgent = require('../../middlewares/authorizeAgent');
 
 const controllers = crud.crudControllers(Wards);
 
-router
-  .route('/:id')
-  .get(
-    passport.authenticate('jwt', { session: false }),
-    authorizeAgent,
-    controllers.getOne
-  );
+router.route('/:id').get(controllers.getOne);
 
 module.exports = router;
