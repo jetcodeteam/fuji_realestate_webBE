3f4ba0d55075796f91f8c6ce37627d56d1610f4f
Shekcon <quangsang9773@gmail.com>
2019-10-21 13:45:07 +0700

    + Fix wards bad request

--- src/apis/locations/ward.js
+++ src/apis/locations/ward.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const WardSchema = new mongoose.Schema(
   {
+    _id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
     name: String,
     type: String,
     slug: String,
