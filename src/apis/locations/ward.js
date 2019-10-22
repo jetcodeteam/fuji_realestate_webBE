b7cf82c8e8340d8d602fd873ab62338693e59a40
Shekcon <quangsang9773@gmail.com>
2019-10-23 00:39:50 +0700

    + Fix get ward by id

--- src/apis/locations/ward.js
+++ src/apis/locations/ward.js
@@ -1,23 +1,32 @@
 const mongoose = require('mongoose');
 
-const WardSchema = new mongoose.Schema(
-  {
-    name: String,
-    type: String,
-    slug: String,
-    name_with_type: String,
-    path: String,
-    path_with_type: String,
-    code: { type: Number, unique: true, dropDups: true },
-    district: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Districts',
-    },
-    parent_code: Number,
+// const WardSchema = new mongoose.Schema(
+//   {
+//     name: String,
+//     type: String,
+//     slug: String,
+//     name_with_type: String,
+//     path: String,
+//     path_with_type: String,
+//     code: { type: Number, unique: true, dropDups: true },
+//     district: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'Districts',
+//     },
+//     parent_code: Number,
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+const WardSchema = new mongoose.Schema({
+  _id: {
+    type: Number,
   },
-  {
-    timestamps: true,
-  }
-);
+  wardList: {
+    type: Array,
+  },
+});
 
 module.exports = mongoose.model('Wards', WardSchema);
