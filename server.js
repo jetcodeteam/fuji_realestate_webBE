fef5401b830e4ba2b6aace99bfaac683e8a392a4
Shekcon <quangsang9773@gmail.com>
2020-01-06 14:07:28 +0700

    + Fix connect api server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./src/app');
 // define port
 const PORT = process.env.PORT || '3000';
 
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, () => {
   return console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT} 🚀`
   );
